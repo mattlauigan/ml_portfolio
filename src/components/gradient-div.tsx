457a19c1dd36372dddec1806eb8b74bd9f0af8a0
Matt Lauigan <mat.lauigan@corewaretech.com.ph>
2024-09-02 07:03:26 +0800

    working on scroll view

--- src/components/gradient-div.tsx
+++ src/components/gradient-div.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { useRef } from "react";
 import useScroll from "../store/scroll";
 
@@ -25,14 +25,47 @@ const GradientDiv: React.FC<GradientDivProps> = ({ children, className }) => {
   };
 
   const setScrollPosition = useScroll((state) => state.setScrollTop);
+  const setCurrentSection = useScroll((state) => state.setCurrentSection);
   const scrollRef = useRef(null);
+  const topContainer = document.getElementById("top-div") as HTMLElement;
+  const xpContainer = document.getElementById(
+    "experience-container"
+  ) as HTMLElement;
+  const projectContainer = document.getElementById(
+    "project-container"
+  ) as HTMLElement;
+
+  function isInViewPort(element: HTMLElement) {
+    const rect = element.getBoundingClientRect();
+
+    return (
+      rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight)
+    );
+  }
 
   const handleScroll = () => {
     if (scrollRef.current) {
-      const { scrollTop } = scrollRef.current;
-      setScrollPosition(scrollTop);
+      const { scrollTop } = scrollRef.current
+      setScrollPosition(scrollTop)
+    }
+
+    if (isInViewPort(topContainer) === true) {
+      console.log(topContainer.id)
+      setCurrentSection(topContainer.id)
+    }
+
+    if (isInViewPort(xpContainer) === true) {
+      console.log(xpContainer.id)
+      setCurrentSection(xpContainer.id)
+    }
+    if (isInViewPort(projectContainer) === true) {
+      console.log(xpContainer.id)
+      setCurrentSection(projectContainer.id)
     }
   };
+
   return (
     <div
       ref={scrollRef}
